1cf12154fb877f8ebd6a609a813e7cedf7e1b926
Aditya <kumarpavanaditya@gmail.com>
2025-02-20 14:54:40 +0530

    done1

--- main.py
+++ main.py
@@ -14,9 +14,6 @@ import random
 import numpy as np
 import psutil 
 import subprocess
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 
